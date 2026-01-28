976b3b18cca9cfb1f89c077d794d28cbf15e555f
Borovskyi Bohdan <borovskiybohdan@gmail.com>
2026-01-28 21:58:56 +0200

    need to change components structure to use tailwind and see in real time how the UI looks like

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./tailwind.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
+
+function render() {
+  if (process.env.NODE_ENV === "development") {
+    return function () {
+      return <h1 className="text-blue">Hello world</h1>;
+    };
+  }
+  return require("./App").default;
+}
+
+const App = render();
 root.render(
   <React.StrictMode>
     <App />
