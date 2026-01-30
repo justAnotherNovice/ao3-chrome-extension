39dc916177dab85fc87a1a5993d894e78f83169c
Borovskyi Bohdan <borovskiybohdan@gmail.com>
2026-01-30 13:58:55 +0200

    updated webpack config to bundle image assets in the build

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
                     "css-loader"
                 ]
             },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loader: 'file-loader'
+            }
         ],
     },
     plugins: [
